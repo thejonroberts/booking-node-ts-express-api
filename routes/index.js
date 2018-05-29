e59b49972056a5ff45900a7de5f52130d7f8388c
Jon Roberts <jon.roberts.8@gmail.com>
2018-05-29 11:36:26 -0500

    / route return test data object json

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@
 const { Router } = require('express');
 const router = Router();
 
-// latest products shown at root
-
-router.get('/', () => {
-  console.log('root');
+router.get('/', (req, res, next) => {
+  const testData = {
+    key: "data",
+    keyAgain: "other Data",
+    fakeArray: [1, 2, 3],
+  }
+  console.log('root', testData);
+  res.json(testData);
+  next();
 });
 
 // pipe all other requests through the route modules

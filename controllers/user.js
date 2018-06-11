733d6ea01513fbffc454cf95cc9f730c7d1761e3
Jon Roberts <jon.roberts.8@gmail.com>
2018-06-10 21:06:22 -0500

    user crud

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,55 @@ module.exports.getAll = (req, res, next) => {
   const { User } = req.app.get('models');
   User.findAll()
     .then(data => {
-      res.json(data);
+      res.status(200).json(data);
     })
-    .catch(next);
+    .catch(err => {
+      next(err);
+    });
+};
+
+module.exports.create = (req, res, next) => {
+  const { User } = req.app.get('models');
+  const user = new User(req.body);
+  user
+    .save()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+};
+
+module.exports.getId = (req, res, next) => {
+  const { User } = req.app.get('models');
+  User.findById(req.params.id, {})
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      next(err);
+    });
+};
+
+module.exports.updateId = (req, res, next) => {
+  const { User } = req.app.get('models');
+  User.update(req.body, { returning: true, where: { id: req.params.id } })
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+};
+
+module.exports.deleteId = (req, res, next) => {
+  const { User } = req.app.get('models');
+  User.destroy({ returning: true, where: { id: req.params.id } })
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
 };
--- test/user.test.js
+++ test/user.test.js
@@ -14,3 +14,88 @@ describe('GET /users', function() {
       });
   });
 });
+
+let createdId;
+
+describe('POST /users', function() {
+  it('responds with created user', function(done) {
+    const newUser = {
+      firstName: 'Josh',
+      lastName: 'Test',
+      email: 'josh@josh.com',
+      username: 'joshrips',
+      password: 'insecure',
+      phoneNumber: '5555555555',
+      lastLoginDate: new Date('December 17, 2016 02:00:00').toISOString(),
+      AddressId: 1,
+    };
+
+    request
+      .post('/users')
+      .set('Accept', 'application/json')
+      .send(newUser)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.body).to.be.an('object');
+        createdId = parseInt(res.body.id, 10);
+        expect(res.body.name).to.equal(newUser.name);
+        done();
+      });
+  });
+});
+
+const userUpdate = {
+  firstName: 'Josh',
+  lastName: 'Test',
+  email: 'josh-new-email@josh.com',
+  username: 'joshrips',
+  password: 'worse',
+  phoneNumber: '7777777777',
+  lastLoginDate: new Date().toISOString(),
+};
+
+describe('PATCH /users', function() {
+  it('updates the given user id', function(done) {
+    request
+      .patch(`/users/${createdId}`)
+      .set('Accept', 'application/json')
+      .send(userUpdate)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.body[1][0]).to.be.an('object');
+        expect(res.body[1][0].name).to.equal(userUpdate.name);
+        done();
+      });
+  });
+
+  it('returns 0 rows updated for non-existent id', function(done) {
+    request
+      .patch(`/users/${createdId + 10}`)
+      .set('Accept', 'application/json')
+      .send(userUpdate)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.body[0]).to.be.equal(0);
+        done();
+      });
+  });
+});
+
+describe('DELETE /users', function() {
+  it('deletes the given user id', function(done) {
+    request
+      .delete(`/users/${createdId}`)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.body[0]).to.be.an('object');
+        expect(res.body[0].name).to.equal(userUpdate.name);
+        expect(res.body[0].deletedAt).to.be.a('string');
+        done();
+      });
+  });
+});

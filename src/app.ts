a8e91af13ddadc740b4c9a7e5d56336ea99d6074
Jon Roberts <jon.roberts.8@gmail.com>
2018-06-17 03:28:52 -0500

    ts for app.ts express args

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import { NextFunction, Request, Response } from 'express';
 import session from 'express-session';
 import validator from 'express-validator';
 import passport from 'passport';
@@ -26,19 +27,21 @@ class App {
     this.app.set('models', models);
     // HTTP Headers
     // Enable CORS from client-side - TODO check best practices / security
-    this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header(
-        'Access-Control-Allow-Methods',
-        'PUT, GET, POST, DELETE, OPTIONS'
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials'
-      );
-      res.header('Access-Control-Allow-Credentials', 'true');
-      next();
-    });
+    this.app.use(
+      (req: Request, res: Response, next: NextFunction): void => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+          'Access-Control-Allow-Methods',
+          'PUT, GET, POST, DELETE, OPTIONS'
+        );
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials'
+        );
+        res.header('Access-Control-Allow-Credentials', 'true');
+        next();
+      }
+    );
     // body-parser
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(bodyParser.json());
@@ -53,20 +56,30 @@ class App {
 
     this.app.use(passport.initialize());
     this.app.use(passport.session());
-    this.app.use((req, res, next) => {
-      res.locals.session = req.session;
-      next();
-    });
+    this.app.use(
+      (req: Request, res: Response, next: NextFunction): void => {
+        res.locals.session = req.session;
+        next();
+      }
+    );
     // TODO express-validator
     this.app.use(validator());
 
-    this.app.use((error, request, res, next) => {
-      if (error) {
-        res.status(500).json({ request, error });
-      } else {
-        next();
+    // TODO: format of move this error handler
+    this.app.use(
+      (
+        error: any,
+        request: Request,
+        res: Response,
+        next: NextFunction
+      ): void => {
+        if (error) {
+          res.status(500).json({ error, request });
+        } else {
+          next();
+        }
       }
-    });
+    );
   }
 
   private routes(): void {
@@ -76,13 +89,13 @@ class App {
 
 export default new App().app;
 
-// TODO: more middlewares:
+// TODO: more middleware:
 // const logger = require('debug')('logger');
 
 // static assets
 // app.use('/public', express.static(__dirname + '/static'));
 
-// DOCS - TODO - swagger-ui & swaager-jsonDoc
+// DOCS - TODO - swagger-ui & swagger-jsonDoc
 // swaggerUi  require('swagger-ui-express');
 // const swaggerDocument = require('./config/swagger.json');
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));

78ce9ff55be5bcc2e7c408bded1b3b738b34c315
Jon Roberts <jon.roberts.8@gmail.com>
2018-06-28 14:53:22 -0500

    check for production mode before using dotenv

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,5 +1,7 @@
-const dotenv = require('dotenv');
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv');
+  dotenv.config();
+}
 const env = process.env.NODE_ENV || 'production';
 const port = process.env.DB_PORT || 5432;
 const dialect = process.env.DIALECT || 'postgres';

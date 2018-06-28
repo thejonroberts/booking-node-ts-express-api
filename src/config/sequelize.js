22694c1d41e90da70704af35a0c38e156b8f2bae
Jon Roberts <jon.roberts.8@gmail.com>
2018-06-28 13:39:30 -0500

    fix sequelize config, travis.yml

---
+++ src/config/sequelize.js
@@ -0,0 +1,49 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const env = process.env.NODE_ENV || 'production';
+const port = process.env.DB_PORT || 5432;
+const dialect = process.env.DIALECT || 'postgres';
+const database = process.env.DB_NAME || 'booking';
+const user = process.env.DB_USER || 'postgres';
+const password = process.env.DB_PASS || 'postgres';
+const host = process.env.DB_HOST || 'localhost';
+
+let url;
+switch (env) {
+  case 'production':
+    url = process.env.DATABASE_URL;
+    break;
+  case 'testing':
+  case 'development':
+    url = `postgres://${user}:${password}@${host}:${port}/${database}`;
+    break;
+  default:
+    url = 'postgres://postgres:postgres@localhost:5432/booking';
+}
+
+// NOTE: compatibility with sequelize cli for migrations, etc...
+module.exports = {
+  development: {
+    database,
+    dialect,
+    host,
+    port,
+  },
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
+  },
+  test_travis: {
+    database,
+    dialect,
+    host,
+    port,
+  },
+  testing: {
+    database,
+    dialect,
+    host,
+    port,
+  },
+  // NOTE: this is what is used by the app (src/model/index)
+  url,
+};
